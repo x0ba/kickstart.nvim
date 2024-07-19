2e837b9d759895e77196b4bf8558fefd168aaa19
x0ba <danielxu0307@proton.me>
2024-07-19 00:19:33 -0700

    added some plugins

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-  'nyoom-engineering/oxocarbon.nvim',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'oxocarbon'
+    vim.cmd.colorscheme 'gruvbox'
   end,
 }
